2833dde0f7681d6034a5952c261cc785e88aaaea
gleba <dev@gleb.pw>
2019-12-03 15:09:15 +0300

    Which in their wills count bad what I think good?

--- react/index.d.ts
+++ react/index.d.ts
@@ -1,11 +1,22 @@
-import { AFlow } from 'alak';
-export declare function useFlow<T>(flow: AFlow<T>): [T, AFlow<T>];
-export declare function useComputeFlow<T, U>(flow: AFlow<T>, mixin: (v: T) => U): [U];
-export declare function useFlowFx<T>(flow: AFlow<T>, effectFn: (v: T) => void): [T];
-export declare function useASyncFlow<T, U>(flow: AFlow<T>, mixin?: (v: T) => U): [U, Boolean];
-export declare function useInputFlow<T>(flow: AFlow<T>, effectFn?: (v: T) => void): [T, (e: {
+import { AFlow } from 'alak'
+export declare function useFlow<T>(flow: AFlow<T>): [T, AFlow<T>]
+export declare function useComputeFlow<T, U>(flow: AFlow<T>, computeFn: (v: T) => U): [U]
+export declare function useFlowFx<T>(flow: AFlow<T>, effectFn: (v: T) => void): [T]
+export declare function useASyncFlow<T, U>(flow: AFlow<T>, mixin?: (v: T) => U): [U, Boolean]
+export declare function useInputFlow<T>(
+  flow: AFlow<T>,
+  effectFn?: (v: T) => void,
+): [
+  T,
+  (e: {
     target: {
-        value?: any;
-        checked?: any;
-    };
-}) => void];
+      value?: any
+      checked?: any
+    }
+  }) => void,
+]
+export declare function useOnFlow<T>(
+  flow: AFlow<T>,
+  listingFn: (v: T) => void,
+  ...diff: any[]
+): void
--- react/index.js
+++ react/index.js
@@ -1,101 +1,102 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+const react_1 = require('react')
 function useFlow(flow) {
-    const [state, mutate] = react_1.useState(flow.value);
-    react_1.useEffect(() => {
-        flow.up(mutate);
-        return () => flow.down(mutate);
-    }, [flow]);
-    // if (state != value) mutate(value)
-    return [state, react_1.useCallback(flow, [flow])];
+  const [state, mutate] = react_1.useState(flow.value)
+  react_1.useEffect(() => {
+    flow.up(mutate)
+    return () => flow.down(mutate)
+  }, [flow])
+  // if (state != value) mutate(value)
+  return [state, react_1.useCallback(flow, [flow])]
 }
-exports.useFlow = useFlow;
-function useComputeFlow(flow, mixin) {
-    let lastValue = flow.value;
-    let value = mixin(lastValue);
-    const [state, mutate] = react_1.useState(value);
-    let mutateFx = v => {
-        if (lastValue !== v) {
-            lastValue = v;
-            mutate(mixin(v));
-        }
-    };
-    react_1.useEffect(() => {
-        flow.up(mutateFx);
-        return () => flow.down(mutateFx);
-    }, [flow]);
-    return [state];
+exports.useFlow = useFlow
+function useComputeFlow(flow, computeFn) {
+  let lastValue = flow.value
+  let value = computeFn(lastValue)
+  const [state, mutate] = react_1.useState(value)
+  let mutateFx = v => {
+    if (lastValue !== v) {
+      lastValue = v
+      mutate(computeFn(v))
+    }
+  }
+  react_1.useEffect(() => {
+    flow.up(mutateFx)
+    return () => flow.down(mutateFx)
+  }, [flow])
+  return [state]
 }
-exports.useComputeFlow = useComputeFlow;
+exports.useComputeFlow = useComputeFlow
 function useFlowFx(flow, effectFn) {
-    let lastValue = flow.value;
-    const [state, mutate] = react_1.useState(flow.value);
-    let mutateFx = v => {
-        if (lastValue !== v) {
-            lastValue = v;
-            effectFn(v);
-            mutate(v);
-        }
-    };
-    react_1.useEffect(() => {
-        flow.up(mutateFx);
-        return () => flow.down(mutateFx);
-    }, [flow]);
-    return [state];
+  let lastValue = flow.value
+  const [state, mutate] = react_1.useState(flow.value)
+  let mutateFx = v => {
+    if (lastValue !== v) {
+      lastValue = v
+      effectFn(v)
+      mutate(v)
+    }
+  }
+  react_1.useEffect(() => {
+    flow.up(mutateFx)
+    return () => flow.down(mutateFx)
+  }, [flow])
+  return [state]
 }
-exports.useFlowFx = useFlowFx;
+exports.useFlowFx = useFlowFx
 function useASyncFlow(flow, mixin) {
-    const [state, mutate] = react_1.useState(flow.value);
-    let busy;
-    if (flow.isAsync) {
-        const [now, change] = react_1.useState(false);
-        busy = { now, change };
+  const [state, mutate] = react_1.useState(flow.value)
+  let busy
+  if (flow.isAsync) {
+    const [now, change] = react_1.useState(false)
+    busy = { now, change }
+  }
+  react_1.useEffect(() => {
+    const mutator = v => state !== v && mutate(v)
+    flow.up(mutator)
+    if (busy) flow.on.await(busy.change)
+    return () => {
+      flow.down(mutator)
+      if (busy) flow.off.await(busy.change)
     }
-    react_1.useEffect(() => {
-        const mutator = v => state !== v && mutate(v);
-        flow.up(mutator);
-        if (busy)
-            flow.on.await(busy.change);
-        return () => {
-            flow.down(mutator);
-            if (busy)
-                flow.off.await(busy.change);
-        };
-    }, [flow]);
-    if (busy)
-        return [state, busy.now];
-    else {
-        console.warn('flow [', flow.id, '] - is not an asynchronous');
-        return [state, false];
-    }
+  }, [flow])
+  if (busy) return [state, busy.now]
+  else {
+    console.warn('flow [', flow.id, '] - is not an asynchronous')
+    return [state, false]
+  }
 }
-exports.useASyncFlow = useASyncFlow;
+exports.useASyncFlow = useASyncFlow
 const asEventHandler = e => {
-    if (e.target) {
-        if (e.target.value)
-            return e.target.value;
-        if (e.target.checked)
-            return e.target.checked;
-    }
-    return e;
-};
-function useInputFlow(flow, effectFn) {
-    let lastValue = flow.value;
-    const [state, mutate] = react_1.useState(flow.value);
-    const mutateFx = v => {
-        if (lastValue !== v) {
-            lastValue = v;
-            if (effectFn)
-                effectFn(v);
-            mutate(v);
-        }
-    };
-    const eventHandler = v => mutateFx(asEventHandler(v));
-    react_1.useEffect(() => {
-        flow.up(mutateFx);
-        return () => flow.down(mutateFx);
-    }, [flow]);
-    return [state, eventHandler];
+  if (e.target) {
+    if (e.target.value) return e.target.value
+    if (e.target.checked) return e.target.checked
+  }
+  return e
 }
-exports.useInputFlow = useInputFlow;
+function useInputFlow(flow, effectFn) {
+  let lastValue = flow.value
+  const [state, mutate] = react_1.useState(flow.value)
+  const mutateFx = v => {
+    if (lastValue !== v) {
+      lastValue = v
+      if (effectFn) effectFn(v)
+      mutate(v)
+    }
+  }
+  const eventHandler = v => mutateFx(asEventHandler(v))
+  react_1.useEffect(() => {
+    flow.up(mutateFx)
+    return () => flow.down(mutateFx)
+  }, [flow])
+  return [state, eventHandler]
+}
+exports.useInputFlow = useInputFlow
+function useOnFlow(flow, listingFn, ...diff) {
+  react_1.useEffect(() => {
+    flow.up(listingFn)
+    return () => flow.down(listingFn)
+  }, [flow, ...diff])
+}
+exports.useOnFlow = useOnFlow
