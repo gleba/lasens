a0ac792ce6f5e36804497b8a65e61641b9d4d29b
Gleb Y Panteleev <dev@gleb.pw>
2020-08-10 08:28:16 +0300

    typings fix

--- packages/sens/index.d.ts
+++ packages/sens/index.d.ts
@@ -85,7 +85,7 @@ type LosPrivateFrom<T> = T extends { _private: any }
 //   // actions: DomainActions<D>
 // }
 
-type Thing<X> = LosClarify<LosAtomized<RmFunc<X>> & PublicThing<X>>
+type Thing<X> = OnlyFunc<X> & LosAtomized<RmFunc<X>> & PublicThing<X>
 
 interface PublicThing<X> {
   $: {
